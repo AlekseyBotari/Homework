3e6f9a47c52246c9b68de51eef4f6e2db37e9ef7
Aleksey <hydrokinetics745@gmail.com>
2022-10-24 19:57:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print('Hello world')  # output 'Hello world'
+
+print('')
+
+part_1 = '#####'
+
+print(str(part_1))
+
+print('')
+
+Part_2 = """
+#####
+#####
+#####
+#####
+#####
+"""
+
+print(str(Part_2))
